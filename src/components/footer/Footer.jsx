6441d7c28cb5b027cfb0c6c777ddacba3be3a310
kalinin <prozaik81-2@yandex.ru>
2020-11-07 14:46:20 +0300

    clear completed ok

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { deleteTodoThunk } from '../../redux/actions';
+
 function Footer(props) {
+  function deleteCompletedTodos() {
+    props.completedTodos.forEach(todo => {
+      props.deleteTodoThunk(todo.id);
+    });
+  }
+
   return (
     <>
-      <div class="cell left">{ props.todos.length } item left</div>
+      <div class="cell left">{ props.notCompletedTodos.length } item left</div>
 
       <div class="cell center">
         <button id="showAll">All</button>
@@ -13,7 +21,7 @@ function Footer(props) {
       </div>
 
       <div class="cell right">
-        <button id="clearCompleted">clear completed</button>
+        <button id="clearCompleted" onClick={ deleteCompletedTodos }>clear completed</button>
       </div>
     </>
   );
@@ -21,8 +29,9 @@ function Footer(props) {
 
 const mapStateToProps = state => {
   return {
-    todos: state.todosReducer.todos.filter(todo => todo.isCompleted === false)
+    notCompletedTodos: state.todosReducer.todos.filter(todo => todo.isCompleted === false),
+    completedTodos: state.todosReducer.todos.filter(todo => todo.isCompleted === true),
   }
 }
 
-export default connect(mapStateToProps, {})(Footer);
+export default connect(mapStateToProps, { deleteTodoThunk })(Footer);
